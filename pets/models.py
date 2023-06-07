7989eebd36a6fd42586f86b038b45cac177d5ab0
hboppe <hanna.nascimento31@gmail.com>
2023-06-07 15:34:44 -0400

    feat: create Pet model and migration

--- pets/models.py
+++ pets/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Gender(models.TextChoices):
+    MALE = "Male"
+    FEMALE = "Female"
+    DEFAULT = "Not Informed"
+
+
+class Pet(models.Model):
+    name = models.CharField(max_length=50)
+    age = models.IntegerField()
+    weight = models.FloatField()
+    sex = models.CharField(
+        max_length=20,
+        choices=Gender.choices,
+        default=Gender.DEFAULT,
+    )
