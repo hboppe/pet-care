27c0a5e569236d24d176b66091aa69a505441a45
hboppe <hanna.nascimento31@gmail.com>
2023-06-08 10:02:35 -0400

    refactor: add traits ManyToManyField

--- pets/models.py
+++ pets/models.py
@@ -19,8 +19,13 @@ class Pet(models.Model):
 
     group = models.ForeignKey(
         "groups.Group",
-        on_delete=models.CASCADE,
+        on_delete=models.PROTECT,
         related_name="pets"
+    )
+
+    traits = models.ManyToManyField(
+        "traits.Trait",
+        related_name="pets",
     )
 
     def __repr__(self) -> str:
