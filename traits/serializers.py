5df08364915340bd61861963c95f6291c51d23a1
hboppe <hanna.nascimento31@gmail.com>
2023-06-09 11:21:45 -0400

    fix: change serializer from ModelSerializer to Serializer

--- traits/serializers.py
+++ traits/serializers.py
@@ -1,12 +1,8 @@
 from rest_framework import serializers
-from .models import Trait
 
-class TraitSerializer(serializers.ModelSerializer):
-    # id = serializers.IntegerField(read_only=True)
-    # created_at = serializers.DateField(read_only=True)
+class TraitSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    trait_name = serializers.CharField(source="name")
+    created_at = serializers.DateTimeField(read_only=True)
 
-    class Meta:
-        model = Trait
-        field = "__all__"
-        read_only_fields = ["id", "created_at"]
 
