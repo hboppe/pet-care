4083c2745165db710a2d5da707f5c4bb455a2461
hboppe <hanna.nascimento31@gmail.com>
2023-06-09 11:23:44 -0400

    fix: move ForeignKeyField to Pet model

--- traits/models.py
+++ traits/models.py
@@ -4,8 +4,4 @@ class Trait(models.Model):
     name = models.CharField(max_length=20, unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
     
-    pets = models.ManyToManyField(
-        "pets.Pet",
-        related_name="traits",
-    )
 
